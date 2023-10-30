320b2f3ad4b28d05df5c85f78b3917b733f31dd4
ghassen hedhli <ghassenhedhli6@gmail.com>
2023-10-30 16:46:53 +0100

    welcome

--- controllers/book.js
+++ controllers/book.js
@@ -1,43 +1,53 @@
 import { categoryModel, authorModel, bookModel } from "../models/book.js";
+import mongoose from "mongoose";
+import { ObjectId } from "mongoose";
+
 
 export const addBook = async (req, res) => {
   try {
     const existingBook = await bookModel.findOne({ ref: req.body.ref });
+
     if (!existingBook) {
-      const newBook = await bookModel.create(req.body);
-
-      // Create an author and associate it with the book
-      const newAuthor = await authorModel.create({
-        book: newBook._id,
-        lastName: req.body.lastName,
-        firstName: req.body.firstName,
-        nationality: req.body.nationality,
-        // You can add other author fields here
-      });
-
-      // Update the book with the author reference
-      await bookModel.findByIdAndUpdate(newBook._id, { author: newAuthor._id });
-
-      // Associate the book with the category
-      const category = await categoryModel.findOneAndUpdate(
-        { title: req.body.category }, // Assuming category is identified by title
-        { $push: { books: newBook._id } }, // Add the new book to the category's books array
-        { new: true }
+      const author = await authorModel.findOneAndUpdate(
+        {
+          lastName: req.body.author.lastName,
+          firstName: req.body.author.firstName,
+          nationality: req.body.author.nationality,
+        },
+        {},
+        { upsert: true, new: true }
       );
+
+      const category = await categoryModel.findOneAndUpdate(
+        { title: req.body.category.title },
+        { title: req.body.category.title },
+        { upsert: true, new: true }
+      );
+
+      const newBook = await bookModel.create({
+        title: req.body.title,
+        ref: req.body.ref,
+        author: author._id,
+        dateref: req.body.dateref,
+        NumofBook: req.body.NumofBook,
+        available: req.body.available,
+        category: category._id,
+      });
 
       return res.status(201).json({
         bookData: newBook,
-        authorData: newAuthor,
-        categoryData: category,
-        message: 'Book, Author, and Category added successfully',
+        message: 'Book added successfully',
       });
     }
 
     res.status(201).json({ message: 'Book already exists' });
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
+
+
+
 
 export const getBook = async (req, res) => {
   try {
@@ -80,8 +90,8 @@ export const deleteBook = async (req, res) => {
 
     // Remove the book from the category's books array
     await categoryModel.findOneAndUpdate(
-      { title: deletedBook.category }, // Assuming category is identified by title
-      { $pull: { books: deletedBook._id } } //to remove the deleted book's ID from the books array of the category
+      { _id: deletedBook.category }, // Assuming category is identified by _id
+      { $pull: { books: deletedBook._id } } // Remove the deleted book's ID from the books array of the category
     );
 
     res.status(200).json({ message: 'Book, Author, and Category deleted successfully' });
